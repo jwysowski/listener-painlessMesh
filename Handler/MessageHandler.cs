f9ece95d935bc2dbccf84d306e18ec6c9b9c2ebe
Jakub Wysowski <kbwysowski@gmail.com>
2022-05-21 10:03:08 +0200

    Round trip test

--- Handler/MessageHandler.cs
+++ Handler/MessageHandler.cs
@@ -1,6 +1,7 @@
 using MQTTnet;
 using Listener.Models;
 using MessageTypes;
+using System.Diagnostics;
 
 namespace MessageHandler
 {
@@ -9,12 +10,16 @@ namespace MessageHandler
         private readonly ListenerContext _context;
         private readonly MessageTypesLUT _typesLUT;
         private bool _isCommand;
+        private long _start;
+        private long _end;
 
         public Handler(ListenerContext context)
         {
             _context = context;
             _typesLUT = new MessageTypesLUT();
             _isCommand = false;
+            _start = 0;
+            _end = 0;
         }
         public async Task Handle(MqttApplicationMessageReceivedEventArgs msg)
         {
@@ -36,9 +41,19 @@ namespace MessageHandler
             log.NodeId = log.Message.Substring(7, 10);
             log.Type = _typesLUT.GetMessageType(log.Message[1]);
             if (log.Type != "Temperature" && log.Type != "Humidity")
+            {
                 _isCommand = true;
+                _start = Stopwatch.GetTimestamp();
+            }
             else
+            {
                 _isCommand = false;
+                _end = Stopwatch.GetTimestamp();
+                var ts = _end - _start;
+                double ts_d = Convert.ToDouble(ts);
+                ts_d /= Stopwatch.Frequency;
+                Console.WriteLine("Elapsed time: {0:N5}", ts_d);
+            }
 
             log.Value = Convert.ToDouble(log.Message.Substring(2, 5));
             log.Timestamp = System.DateTime.Now;
